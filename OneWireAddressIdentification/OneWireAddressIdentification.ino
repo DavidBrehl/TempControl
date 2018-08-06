530b870e09305af90c7612c569ac3c729a21e912
DavidBrehl <37316500+DavidBrehl@users.noreply.github.com>
2018-08-05 23:58:34 -0400

    Update OneWireAddressIdentification.ino

--- OneWireAddressIdentification/OneWireAddressIdentification.ino
+++ OneWireAddressIdentification/OneWireAddressIdentification.ino
@@ -8,15 +8,15 @@ OneWire oneWire(ONE_WIRE_BUS);
 
 DallasTemperature sensors(&oneWire);
 
-DeviceAddress Therm0;
+DeviceAddress Therm;
 
 void setup() {
   Serial.begin(9600);
   sensors.begin();
-  sensors.getAddress(Therm0, 0);
-  sensors.setResolution(Therm0, TEMPERATURE_PRECISION);
-  Serial.print ("Therm0 address: ");
-  printAddress(Therm0);
+  sensors.getAddress(Therm, 0);
+  sensors.setResolution(Therm, TEMPERATURE_PRECISION);
+  Serial.print ("Therm address: ");
+  printAddress(Therm);
 }
 
 void loop() { 
