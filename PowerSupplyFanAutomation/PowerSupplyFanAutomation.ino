86222ac8b05c97a139f2b33b6274befacc8a41f2
DavidBrehl <37316500+DavidBrehl@users.noreply.github.com>
2018-09-01 11:14:19 -0400

    Add files via upload

--- PowerSupplyFanAutomation/PowerSupplyFanAutomation.ino
+++ PowerSupplyFanAutomation/PowerSupplyFanAutomation.ino
@@ -9,6 +9,7 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 //declare the device addresses for each temperature sensor; it's in an array, so the index of OutT is 0, InT0 is 1, InT1 is 2, InT2 is 3.
 //This is the only thing you have to worry about when replacing and configuring the temperature sensors, and I think you can figure out the formatting.
+//If anything falls out, digital pin 2 is for the sensors, digital pin 3 is overheat, and the fan plugs into the relay shield's COM1 and NO1
 DeviceAddress tempSensorSerial[4]= {
   {0x28, 0xFF, 0x63, 0x40, 0x20, 0x17, 0x03, 0x03}, 
   {0x28, 0xFF, 0x42, 0xA3, 0x20, 0x17, 0x04, 0x74},
@@ -20,6 +21,7 @@ float InT0;
 float InT1;
 float InT2;
 bool Fan;
+float Overheat = 50; //temperature threshold at which to flash a warning light if any inside temperature sensors pass it
 
 void setup() {
   Serial.begin(9600);
@@ -49,5 +51,6 @@ void loop() {
     Serial.println("Fan OFF");
     digitalWrite(7, LOW);
   }
+  digitalWrite(3, ((InT0 >= Overheat) || (InT1 >= Overheat) || (InT2 >= Overheat)));
   delay (500);
 }
